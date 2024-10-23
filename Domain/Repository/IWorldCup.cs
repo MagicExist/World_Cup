a9ae305d312b585326926a006d8c378393e6f0a0
target-id <codeberrio@gmail.com>
2024-10-23 11:10:26 -0500

    Add interface "GetChampionshipAsync"

---
+++ Domain/Repository/IWorldCup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using World_Cup.DataBase;
+
+namespace Domain.Repository
+{
+    public interface IWorldCup
+    {
+        Task<ChampionShip> GetChampionShipAsync();
+    }
+}
